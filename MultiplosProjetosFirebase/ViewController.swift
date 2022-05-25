9ad97df33499b3a26f916d8ff0328540a99efd07
Helio Mesquita <helio.mesquitaios@gmail.com>
2022-05-25 15:40:33 -0300

    Initial Commit

---
+++ MultiplosProjetosFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiplosProjetosFirebase
+//
+//  Created by Hélio Mesquita on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
